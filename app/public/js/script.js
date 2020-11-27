d6c30ab687cfd71165d022751f88d60bee2e24c7
emekarr <aceanyanwu02@gmail.com>
2020-11-27 22:45:38 +0100

    created navbar and made it responsive. created signup and login page and started the express part and created routes for index, login and signup

---
+++ app/public/js/script.js
@@ -0,0 +1,60 @@
+const hamburgerButton = document.querySelector(".fa-bars");
+const closeMenuButton = document.querySelector(".fa-close");
+const navMenu = document.querySelector("nav");
+//form submit button
+const submitButton = document.querySelector(".form > button");
+
+const navBarController = (moveIn, moveOut, action1, action2) => {
+  hamburgerButton.classList.remove(moveIn);
+  closeMenuButton.classList.remove(moveOut);
+  hamburgerButton.classList.add(moveOut);
+  closeMenuButton.classList.add(moveIn);
+  navMenu.classList.remove(action2);
+  navMenu.classList.add(action1);
+};
+
+submitButton.addEventListener("click", (event) => {
+  const formFields = document.querySelectorAll(".form > input");
+  const usernameField = formFields[0];
+  const passwordField1 = formFields[1];
+  const passwordField2 = formFields[2];
+
+  if (passwordField1.value !== passwordField2.value) {
+    event.preventDefault();
+    const authFailedBanner = document.querySelector(".auth-failed-banner");
+    authFailedBanner.firstElementChild.innerHTML =
+      "Make sure your passwords match!";
+    authFailedBanner.classList.add("show-banner");
+  } else if (passwordField1.value.length < 5) {
+    event.preventDefault();
+    const authFailedBanner = document.querySelector(".auth-failed-banner");
+    authFailedBanner.firstElementChild.innerHTML =
+      "Make sure your password is more than 4 digits";
+    authFailedBanner.classList.add("show-banner");
+  } else if (usernameField.value.includes(" ")) {
+    event.preventDefault();
+    const authFailedBanner = document.querySelector(".auth-failed-banner");
+    authFailedBanner.firstElementChild.innerHTML =
+      "Username must not contain any white spaces!";
+    authFailedBanner.classList.add("show-banner");
+  }
+
+//   fetch("/signup", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//         name: usernameField.value,
+//         password: passwordField1.value,
+//     }),
+//   });
+});
+
+hamburgerButton.addEventListener("click", () => {
+  navBarController("move-in", "move-out", "nav-open", "nav-close");
+});
+
+closeMenuButton.addEventListener("click", () => {
+  navBarController("move-out", "move-in", "nav-close", "nav-open");
+});
---
+++ app/server.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const path = require("path")
+
+const viewsLocation = path.join(__dirname, "./templates/views")
+const staticFilesLocation = path.join(__dirname, "./public")
+
+const server = express()
+
+server.use(express.json())
+server.use(express.urlencoded({extended: true}))
+
+server.set("view engine", "ejs")
+server.set("views", viewsLocation)
+
+server.use(express.static(staticFilesLocation))
+
+server.get("/", (req, res)=> {
+    res.render("index")
+})
+
+server.get("/login", (req, res) => {
+    res.render("login")
+})
+
+server.post("/login", (req, res) => {
+    res.send(req.body)
+})
+
+server.get("/signup", (req, res) => {
+    res.render("signup")
+})
+
+server.post("/signup", (req, res) => {
+    res.send(req.body)
+})
+
+server.get("*", (req, res) => {
+    res.status(404)
+    res.render("404", {url: req.url})
+})
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT)
