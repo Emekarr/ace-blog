1a7473402d08c25296ce124794f0e30b18c29b59
emekarr <aceanyanwu02@gmail.com>
2020-11-28 13:36:28 +0100

    created admins page and separated the code in style.css into different files for specific pages

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -13,43 +13,34 @@ const navBarController = (moveIn, moveOut, action1, action2) => {
   navMenu.classList.add(action1);
 };
 
-submitButton.addEventListener("click", (event) => {
-  const formFields = document.querySelectorAll(".form > input");
-  const usernameField = formFields[0];
-  const passwordField1 = formFields[1];
-  const passwordField2 = formFields[2];
-
-  if (passwordField1.value !== passwordField2.value) {
-    event.preventDefault();
-    const authFailedBanner = document.querySelector(".auth-failed-banner");
-    authFailedBanner.firstElementChild.innerHTML =
-      "Make sure your passwords match!";
-    authFailedBanner.classList.add("show-banner");
-  } else if (passwordField1.value.length < 5) {
-    event.preventDefault();
-    const authFailedBanner = document.querySelector(".auth-failed-banner");
-    authFailedBanner.firstElementChild.innerHTML =
-      "Make sure your password is more than 4 digits";
-    authFailedBanner.classList.add("show-banner");
-  } else if (usernameField.value.includes(" ")) {
-    event.preventDefault();
-    const authFailedBanner = document.querySelector(".auth-failed-banner");
-    authFailedBanner.firstElementChild.innerHTML =
-      "Username must not contain any white spaces!";
-    authFailedBanner.classList.add("show-banner");
-  }
-
-//   fetch("/signup", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//         name: usernameField.value,
-//         password: passwordField1.value,
-//     }),
-//   });
-});
+if (submitButton){
+  submitButton.addEventListener("click", (event) => {
+    const formFields = document.querySelectorAll(".form > input");
+    const usernameField = formFields[0];
+    const passwordField1 = formFields[1];
+    const passwordField2 = formFields[2];
+  
+    if (passwordField1.value !== passwordField2.value) {
+      event.preventDefault();
+      const authFailedBanner = document.querySelector(".auth-failed-banner");
+      authFailedBanner.firstElementChild.innerHTML =
+        "Make sure your passwords match!";
+      authFailedBanner.classList.add("show-banner");
+    } else if (passwordField1.value.length < 5) {
+      event.preventDefault();
+      const authFailedBanner = document.querySelector(".auth-failed-banner");
+      authFailedBanner.firstElementChild.innerHTML =
+        "Make sure your password is more than 4 digits";
+      authFailedBanner.classList.add("show-banner");
+    } else if (usernameField.value.includes(" ")) {
+      event.preventDefault();
+      const authFailedBanner = document.querySelector(".auth-failed-banner");
+      authFailedBanner.firstElementChild.innerHTML =
+        "Username must not contain any white spaces!";
+      authFailedBanner.classList.add("show-banner");
+    }
+  });
+}
 
 hamburgerButton.addEventListener("click", () => {
   navBarController("move-in", "move-out", "nav-open", "nav-close");
--- app/server.js
+++ app/server.js
@@ -1,51 +1,56 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 
-const viewsLocation = path.join(__dirname, "./templates/views")
-const staticFilesLocation = path.join(__dirname, "./public")
+const viewsLocation = path.join(__dirname, "./templates/views");
+const staticFilesLocation = path.join(__dirname, "./public");
 
-const server = express()
+const server = express();
 
-server.use(express.json())
-server.use(express.urlencoded({extended: true}))
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
-server.set("view engine", "ejs")
-server.set("views", viewsLocation)
+server.set("view engine", "ejs");
+server.set("views", viewsLocation);
 
-server.use(express.static(staticFilesLocation))
+server.use(express.static(staticFilesLocation));
 
-server.get("/", (req, res)=> {
-    res.render("index")
-})
+server.get("/", (req, res) => {
+  const articles = [];
+  res.render("index", { articles });
+});
+
+server.get("/admin", (req, res) => {
+  res.render("admin");
+});
 
 server.get("/contact", (req, res) => {
-    res.render("contact")
-})
+  res.render("contact");
+});
 
 server.post("/contact", (req, res) => {
-    res.send(req.body)
-})
+  res.send(req.body);
+});
 
 server.get("/login", (req, res) => {
-    res.render("login")
-})
+  res.render("login");
+});
 
 server.post("/login", (req, res) => {
-    res.send(req.body)
-})
+  res.send(req.body);
+});
 
 server.get("/signup", (req, res) => {
-    res.render("signup")
-})
+  res.render("signup");
+});
 
 server.post("/signup", (req, res) => {
-    res.send(req.body)
-})
+  res.send(req.body);
+});
 
 server.get("*", (req, res) => {
-    res.status(404)
-    res.render("404", {url: req.url})
-})
+  res.status(404);
+  res.render("404", { url: req.url });
+});
 
-const PORT = process.env.PORT || 3000
-server.listen(PORT)
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
